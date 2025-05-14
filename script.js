05c3d2f8efaecbb790144e5f906867e7b5319488
Rashika2308 <rashikavidyarthi545@gmail.com>
2025-05-14 13:05:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+	function divide(arr, n) {
+  const result = [];
+  let currentSubarray = [];
+  let currentSum = 0;
+
+  for (let num of arr) {
+    if (currentSum + num <= n) {
+      currentSubarray.push(num);
+      currentSum += num;
+    } else {
+      result.push(currentSubarray);
+      currentSubarray = [num];
+      currentSum = num;
+    }
+  }
+
+  if (currentSubarray.length > 0) {
+    result.push(currentSubarray);
+  }
+
+  return result;
+}
+console.log(divide([1, 2, 3, 4, 1, 0, 2, 2], 5));
+
+console.log(divide([4, 3, 2, 1], 4));
 };
 
 const n = prompt("Enter n: ");
